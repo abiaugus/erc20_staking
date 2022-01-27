0752aa9f93b89f9cffb20dffcaec2aad98685a47
Abhijith <abhi@Abhijiths-MacBook-Air.local>
2022-01-27 12:00:45 +0530

    can create stakes

--- test/sample-test.js
+++ test/sample-test.js
@@ -2,23 +2,58 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("StakingToken", function () {
-  it("Should have 100M initial mint.", async function () {
-    const StakingToken = await ethers.getContractFactory("StakingToken");
-    const stakingtoken = await StakingToken.deploy("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266", "100000000");
-    await stakingtoken.deployed();
+  let StakingToken;
+  let stakingtoken;
+  let owner;
+  let stakeHolderA;
+  let stakeHolderASign;
+  let stakeHolderB;
+  beforeEach( async () => {
+    const signers = await ethers.getSigners();
+    owner = signers[0].address;
+    stakeHolderA = signers[2].address;
+    stakeHolderASign = signers[2];
+    // console.log("owner", owner, stakeHolderA);
+    StakingToken = await ethers.getContractFactory("StakingToken");
+    stakingtoken = await StakingToken.deploy(owner, "100000000");
+    // owner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+    // stakeHolderA = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
+  })
 
-    // expect(await greeter.greet()).to.equal("Hello, world!");
-    const balance = await stakingtoken.balanceOf("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-    // console.log("Balance is", balance.toString());
-    const parsedBalance =  balance.toString();
-    console.log(parsedBalance);
-    expect(parsedBalance).to.equal("100000000");
+  describe("Mint", async() => {
+    it("Should have 100M initial mint.", async function () {
+      await stakingtoken.deployed();
+      const balance = await stakingtoken.balanceOf(owner);
+      const parsedBalance =  balance.toString();
+      expect(parsedBalance).to.equal("100000000");
+    });
+  })
 
-    // const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // // wait until the transaction is mined
-    // await setGreetingTx.wait();
-
-    // expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+  describe("Transfer", async() => {
+    it("can transfer tokens", async () => {
+      await stakingtoken.deployed();
+      stakingtoken.transfer(stakeHolderA, "10000000");
+      const balance = await stakingtoken.balanceOf(stakeHolderA);
+      const parsedBalance =  balance.toString();
+      expect(parsedBalance).to.equal("10000000");
+    })
+  })
+  describe("Stakes", async() => {
+    it("can create a stake", async() => {
+      await stakingtoken.deployed();
+      await stakingtoken.transfer(stakeHolderA, 10);
+      await stakingtoken.connect(stakeHolderASign).createStake(5);
+      const balance = await stakingtoken.balanceOf(stakeHolderA);
+      const parsedBalance =  balance.toString();
+      const totalStakes = await stakingtoken.totalStakes();
+      const parsedStakes = totalStakes.toString();
+      expect(parsedStakes).to.equal("5");
+      // const parsedStakeOf = await stakingtoken.stakeOf(owner);
+      // expect(parsedStakeOf).to.equal(5);
+    })
+    // it("can track total stakes", async () => {
+    //   await stakingtoken.deployed();
+      
+    // })
+  })
 });
