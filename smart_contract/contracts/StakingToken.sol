c39f390a1d0fbe684c89203861f29b0a615c9969
Abhijith <abhi@Abhijiths-MacBook-Air.local>
2022-02-01 12:52:23 +0530

    final

--- smart_contract/contracts/StakingToken.sol
+++ smart_contract/contracts/StakingToken.sol
@@ -39,61 +39,61 @@ contract StakingToken is ERC20, Ownable {
            stakeholders.pop();
        }
    }
-    mapping(address => uint256) internal stakes;
+   mapping(address => uint256) internal stakes;
 
-    function stakeOf(address _stakeholder) public view returns(uint256){
-       return stakes[_stakeholder];
-    }
+   function stakeOf(address _stakeholder) public view returns(uint256){
+      return stakes[_stakeholder];
+   }
 
-    function totalStakes() public view returns(uint256) {
+   function totalStakes() public view returns(uint256) {
        uint256 _totalStakes = 0;
        for(uint256 s = 0; s <stakeholders.length; s++){
            _totalStakes = _totalStakes.add(stakes[stakeholders[s]]);
        }
        return _totalStakes;
-    }
+   }
       
-    function createStake(uint256 _stake) public {
+   function createStake(uint256 _stake) public {
        _burn(msg.sender, _stake);
        if(stakes[msg.sender] == 0) addStakeholder(msg.sender);
        stakes[msg.sender] = stakes[msg.sender].add(_stake);
-    }
+   }
 
-    function removeStake(uint256 _stake) public {
+   function removeStake(uint256 _stake) public {
        stakes[msg.sender] = stakes[msg.sender].sub(_stake);
        if(stakes[msg.sender] == 0) removeStakeholder(msg.sender);
        _mint(msg.sender, _stake);
-    }
+   }
 
    mapping(address => uint256) internal rewards;
 
     function rewardOf(address _stakeholder) public view returns(uint256) {
        return rewards[_stakeholder];
-    }
+   }
 
-    function totalRewards() public view returns(uint256) {
+   function totalRewards() public view returns(uint256) {
        uint256 _totalRewards = 0;
         for (uint256 s = 0; s < stakeholders.length; s += 1){
            _totalRewards = _totalRewards.add(rewards[stakeholders[s]]);
         }
         return _totalRewards;
-    }
+   }
 
-    function calculateReward(address _stakeholder) public view returns(uint256) {
+   function calculateReward(address _stakeholder) public view returns(uint256) {
        return stakes[_stakeholder] / 100;
-    }
+   }
 
-    function distributeRewards() public onlyOwner {
+   function distributeRewards() public onlyOwner {
         for (uint256 s = 0; s < stakeholders.length; s += 1){
            address stakeholder = stakeholders[s];
            uint256 reward = calculateReward(stakeholder);
            rewards[stakeholder] = rewards[stakeholder].add(reward);
-        }
-    }
+      }
+   }
 
-    function withdrawReward() public {
+   function withdrawReward() public {
        uint256 reward = rewards[msg.sender];
        rewards[msg.sender] = 0;
        _mint(msg.sender, reward);
-    }
+   }
 }
